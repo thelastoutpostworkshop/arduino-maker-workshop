fb2fb4d308a6b451aa501298d999eef79ce34b43
charles <cgiguere@iname.com>
2024-09-06 23:38:27 -0400

    arduino-cli compile

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const cp = require('child_process')
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Create an output channel
+    const outputChannel = vscode.window.createOutputChannel('Arduino CLI');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-arduino" is now active!');
+    // Register the compile command
+    let disposable = vscode.commands.registerCommand('vscode-arduino.compile', () => {
+        vscode.window.showInformationMessage('Compiling Arduino project...');
+        
+        // Clear the output channel before running the command
+        outputChannel.clear();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-arduino.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSCode Arduino!');
-	});
+        // Append initial message to the output channel
+        outputChannel.appendLine('Running Arduino CLI compile...');
 
-	context.subscriptions.push(disposable);
+        // Show the output channel
+        outputChannel.show(true);
+
+        // Execute the Arduino CLI command
+        cp.exec('arduino-cli compile -v', (error, stdout, stderr) => {
+            if (error) {
+                outputChannel.appendLine(`Error: ${stderr}`);
+                vscode.window.showErrorMessage(`Compilation failed. Check Output window for details.`);
+                return;
+            }
+
+            // Write stdout to the output channel
+            outputChannel.appendLine(stdout);
+
+            // Show a success message in the information box
+            vscode.window.showInformationMessage('Compilation successful.');
+        });
+    });
+
+    // Push the disposable command to the context's subscriptions
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
