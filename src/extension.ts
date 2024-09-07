550392daca577575211ef7572dc4c2e42a2fb16d
charles <cgiguere@iname.com>
2024-09-07 09:46:16 -0400

    executeArduinoCommand

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,12 @@ const cp = require('child_process');
 
 const cliCommandArduino: string = 'arduino-cli';
 
+const outputChannel = vscode.window.createOutputChannel('Arduino');
 
 let arduinoProject: ArduinoProject;
 
 export function activate(context: vscode.ExtensionContext) {
 	// Create an output channel
-	const outputChannel = vscode.window.createOutputChannel('Arduino');
 
 	arduinoProject = new ArduinoProject();
 
@@ -33,39 +33,47 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showInformationMessage('Project path not found, cannot compile');
 		}
 
-		outputChannel.clear();
-		outputChannel.appendLine('Running Arduino CLI compile...');
-		outputChannel.show(true);
 
 		// Execute the Arduino CLI command
 		const compileCommand = arduinoProject.getCompileCommandArguments();
-		outputChannel.appendLine(`${cliCommandArduino}`);
-		outputChannel.appendLine(compileCommand.join(' ')); 
+		executeArduinoCommand(`${cliCommandArduino}`,compileCommand);
 
-		const child = cp.spawn(`${cliCommandArduino}`, compileCommand);
-
-		// Stream stdout to the output channel
-		child.stdout.on('data', (data: Buffer) => {
-			outputChannel.appendLine(data.toString());
-		});
-
-		// Stream stderr to the output channel
-		child.stderr.on('data', (data: Buffer) => {
-			outputChannel.appendLine(`Error: ${data.toString()}`);
-		});
-
-		// Handle the process exit event
-		child.on('close', (code: number) => {
-			if (code === 0) {
-				// vscode.window.showInformationMessage('Compilation successful.');
-			} else {
-				vscode.window.showErrorMessage(`Compilation failed with code ${code}. Check Output window for details.`);
-			}
-		});
 	});
 
 	// Push the disposable command to the context's subscriptions
 	context.subscriptions.push(compileDisposable);
+}
+
+
+function executeArduinoCommand(command: string, args: string[]) {
+
+	outputChannel.clear();
+	outputChannel.show(true);
+	outputChannel.appendLine('Running Arduino CLI...');
+	outputChannel.appendLine(`${cliCommandArduino}`);
+	outputChannel.appendLine(args.join(' '));
+
+	const child = cp.spawn(`${command}`, args);
+
+	// Stream stdout to the output channel
+	child.stdout.on('data', (data: Buffer) => {
+		outputChannel.appendLine(data.toString());
+	});
+
+	// Stream stderr to the output channel
+	child.stderr.on('data', (data: Buffer) => {
+		outputChannel.appendLine(`Error: ${data.toString()}`);
+	});
+
+	// Handle the process exit event
+	child.on('close', (code: number) => {
+		if (code === 0) {
+			// vscode.window.showInformationMessage('Compilation successful.');
+		} else {
+			vscode.window.showErrorMessage(`Compilation failed with code ${code}. Check Output window for details.`);
+		}
+	});
+
 }
 
 export function deactivate() { }
