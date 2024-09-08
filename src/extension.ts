7307cd10778fab5de36088bb946e2d7d0a9aad63
charles <cgiguere@iname.com>
2024-09-08 14:55:17 -0400

    boardConfigWebViewPanel

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,10 @@ const cp = require('child_process');
 
 const outputChannel = vscode.window.createOutputChannel('Arduino');
 let arduinoProject: ArduinoProject;
+let boardConfigWebViewPanel: vscode.WebviewPanel | undefined = undefined;
 
 export function activate(context: vscode.ExtensionContext) {
+
 	loadArduinoConfiguration();
 	context.subscriptions.push(vsCommandCompile());
 	context.subscriptions.push(vsCommandUpload());
@@ -24,109 +26,117 @@ function loadArduinoConfiguration() {
 }
 
 function vsCommandBoardConfiguration(context: vscode.ExtensionContext): vscode.Disposable {
-    return vscode.commands.registerCommand('vscode-arduino.boardconfig', async () => {
-        if (!verifyArduinoProject()) {
-            return;
-        }
+	return vscode.commands.registerCommand('vscode-arduino.boardconfig', async () => {
+		if (!verifyArduinoProject()) {
+			return;
+		}
 
-        if (!arduinoProject.getProjectPath()) {
-            vscode.window.showInformationMessage('Project path not found, cannot retrieve board configuration.');
-            return;
-        }
+		if (!arduinoProject.getProjectPath()) {
+			vscode.window.showInformationMessage('Project path not found, cannot retrieve board configuration.');
+			return;
+		}
 
-        if (!arduinoProject.getBoard()) {
-            vscode.window.showInformationMessage('Board info not found, cannot retrieve board configuration.');
-            return;
-        }
+		if (!arduinoProject.getBoard()) {
+			vscode.window.showInformationMessage('Board info not found, cannot retrieve board configuration.');
+			return;
+		}
 
-        await vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: 'Retrieving board configuration options...',
-            cancellable: false
-        }, async (progress) => {
-            const configBoardArgs = arduinoProject.getBoardConfigurationArguments();
+		await vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: 'Retrieving board configuration options...',
+			cancellable: false
+		}, async (progress) => {
+			const configBoardArgs = arduinoProject.getBoardConfigurationArguments();
 
-            try {
-                const result = await executeArduinoCommand(`${cliCommandArduino}`, configBoardArgs, true);
-                if (result) {
-                    progress.report({ message: 'Board Configuration retrieved.', increment: 100 });
+			try {
+				const result = await executeArduinoCommand(`${cliCommandArduino}`, configBoardArgs, true);
+				if (result) {
+					progress.report({ message: 'Board Configuration retrieved.', increment: 100 });
 
-                    // Parse the JSON result
+					// Parse the JSON result
 					const configData = JSON.parse(result);
 					const configuration = configData.config_options;
 					const boardName = configData.name; // Extract the board name
-										
-                    if (configuration.length === 0) {
-                        vscode.window.showInformationMessage('Unable to retrieve board configuration.');
-                        return;
-                    }
 
-                    // Create a new webview panel
-                    const panel = vscode.window.createWebviewPanel(
-                        'boardConfig', 
-                        'Arduino Board Configuration', 
-                        vscode.ViewColumn.One, 
-                        { enableScripts: true }
-                    );
+					if (configuration.length === 0) {
+						vscode.window.showInformationMessage('Unable to retrieve board configuration.');
+						return;
+					}
 
-                    // Set the HTML content with the configuration options
-                    panel.webview.html = getWebviewContent(boardName,configuration);
+					const columnToShowIn = vscode.window.activeTextEditor
+						? vscode.window.activeTextEditor.viewColumn
+						: undefined;
 
-                    // Handle messages from the webview
-                    panel.webview.onDidReceiveMessage(
-                        message => {
-                            switch (message.command) {
-                                case 'updateConfig':
-                                    // Update configuration in the project
-                                    arduinoProject.updateBoardConfiguration(message.config);
-                                    vscode.window.showInformationMessage('Configuration updated!');
-                                    return;
-                            }
-                        },
-                        undefined,
-                        context.subscriptions
-                    );
-                } else {
-                    vscode.window.showErrorMessage('Failed to retrieve board configuration.');
-                }
-            } catch (error) {
-                vscode.window.showErrorMessage(`Error retrieving board configuration: ${error}`);
-            }
-        });
-    });
+					if (boardConfigWebViewPanel) {
+						boardConfigWebViewPanel.reveal(columnToShowIn);
+					} else {
+						boardConfigWebViewPanel = vscode.window.createWebviewPanel(
+							'boardConfig',
+							'Arduino Board Configuration',
+							columnToShowIn || vscode.ViewColumn.One,
+							{ enableScripts: true }
+						);
+
+					}
+
+					// Set the HTML content with the configuration options
+					boardConfigWebViewPanel.webview.html = getWebviewContent(boardName, configuration);
+
+					// Handle messages from the webview
+					boardConfigWebViewPanel.webview.onDidReceiveMessage(
+						message => {
+							switch (message.command) {
+								case 'updateConfig':
+									// Update configuration in the project
+									arduinoProject.updateBoardConfiguration(message.config);
+									vscode.window.showInformationMessage('Configuration updated!');
+									return;
+							}
+						},
+						undefined,
+						context.subscriptions
+					);
+				} else {
+					vscode.window.showErrorMessage('Failed to retrieve board configuration.');
+				}
+			} catch (error) {
+				vscode.window.showErrorMessage(`Error retrieving board configuration: ${error}`);
+			}
+		});
+	});
 }
 
-function getWebviewContent(boardName: string,configuration: any[]): string {
-    let html = `
+function getWebviewContent(boardName: string, configuration: any[]): string {
+	let html = `
     <html>
     <body>
         <h1>${boardName} - Board Configuration</h1> <!-- Show board name -->
         <form id="configForm">`;
 
-    // Parse the current configuration string into an object
-    const currentConfig = arduinoProject.getConfiguration()
-        .split(',')
-        .reduce((configObj: any, item: string) => {
-            const [key, value] = item.split('=');
-            configObj[key] = value;
-            return configObj;
-        }, {});
+	// Parse the current configuration string into an object
+	const currentConfig = arduinoProject.getConfiguration()
+		.split(',')
+		.reduce((configObj: any, item: string) => {
+			const [key, value] = item.split('=');
+			configObj[key] = value;
+			return configObj;
+		}, {});
 
-    // Iterate over configuration options and create a select input for each
-    configuration.forEach(option => {
-        html += `<label for="${option.option}">${option.option_label}</label>
+	// Iterate over configuration options and create a select input for each
+	configuration.forEach(option => {
+		html += `<label for="${option.option}">${option.option_label}</label>
         <select id="${option.option}" name="${option.option}">`;
 
-        option.values.forEach(value => {
-            // Check if the value matches the current config value for this option
-            const isSelected = currentConfig[option.option] === value.value ? 'selected' : '';
-            html += `<option value="${value.value}" ${isSelected}>${value.value_label}</option>`;
-        });
+		option.values.forEach(value => {
+			// Check if the value matches the current config value for this option
+			const isSelected = currentConfig[option.option] === value.value ? 'selected' : '';
+			html += `<option value="${value.value}" ${isSelected}>${value.value_label}</option>`;
+		});
 
-        html += `</select><br/><br/>`;
-    });
+		html += `</select><br/><br/>`;
+	});
 
-    html += `
+	html += `
         <button type="submit">Update Configuration</button>
         </form>
 
@@ -168,7 +178,7 @@ function getWebviewContent(boardName: string,configuration: any[]): string {
     </body>
     </html>`;
 
-    return html;
+	return html;
 }
 
 
@@ -178,12 +188,12 @@ function vsCommandPort(): vscode.Disposable {
 		if (!verifyArduinoProject()) {
 			return;
 		}
-	
+
 		if (!arduinoProject.getProjectPath()) {
 			vscode.window.showInformationMessage('Project path not found, cannot retrieve ports.');
 			return;
 		}
-	
+
 		await vscode.window.withProgress({
 			location: vscode.ProgressLocation.Notification,
 			title: 'Retrieving available ports...',
@@ -191,32 +201,32 @@ function vsCommandPort(): vscode.Disposable {
 		}, async (progress) => {
 			// Execute the Arduino CLI command to get the list of ports
 			const portListCommand = arduinoProject.getPortListArguments();
-	
+
 			// Use executeArduinoCommand and pass true to get the output as a string
 			try {
 				const result = await executeArduinoCommand(`${cliCommandArduino}`, portListCommand, true);
 				if (result) {
-					progress.report({ message: 'Ports have been successfully retrieved.',increment:100 });
+					progress.report({ message: 'Ports have been successfully retrieved.', increment: 100 });
 
 					// Parse the JSON result
 					const ports = JSON.parse(result).detected_ports;
-	
+
 					if (ports.length === 0) {
 						vscode.window.showInformationMessage('No ports detected.');
 						return;
 					}
-	
+
 					// Create QuickPick items from the list of detected ports
 					const portItems = ports.map((port: any) => ({
 						label: port.port.label,
 						description: port.port.protocol_label
 					}));
-	
+
 					// Show the QuickPick to the user
 					const selectedPort = await vscode.window.showQuickPick(portItems, {
 						placeHolder: 'Select a port to use for uploading'
 					});
-	
+
 					if (selectedPort) {
 						vscode.window.showInformationMessage(`Selected port: ${selectedPort.label}`);
 						// Set the selected port in your project settings or wherever needed
@@ -232,13 +242,13 @@ function vsCommandPort(): vscode.Disposable {
 			}
 		});
 	});
-	
+
 }
 
 
-function vsCommandUpload():vscode.Disposable {
+function vsCommandUpload(): vscode.Disposable {
 	return vscode.commands.registerCommand('vscode-arduino.upload', () => {
-		if(!verifyArduinoProject()) {
+		if (!verifyArduinoProject()) {
 			return;
 		}
 
@@ -257,14 +267,14 @@ function vsCommandUpload():vscode.Disposable {
 
 		// Execute the Arduino CLI command
 		const uploadCommand = arduinoProject.getCompileUploadArguments();
-		const output = executeArduinoCommand(`${cliCommandArduino}`,uploadCommand,true);
+		const output = executeArduinoCommand(`${cliCommandArduino}`, uploadCommand, true);
 
 	});
 }
 
-function vsCommandCompile():vscode.Disposable {
+function vsCommandCompile(): vscode.Disposable {
 	return vscode.commands.registerCommand('vscode-arduino.compile', () => {
-		if(!verifyArduinoProject()) {
+		if (!verifyArduinoProject()) {
 			return;
 		}
 		if (!arduinoProject.getBoard()) {
@@ -282,69 +292,69 @@ function vsCommandCompile():vscode.Disposable {
 
 		// Execute the Arduino CLI command
 		const compileCommand = arduinoProject.getCompileCommandArguments();
-		executeArduinoCommand(`${cliCommandArduino}`,compileCommand);
+		executeArduinoCommand(`${cliCommandArduino}`, compileCommand);
 
 	});
 }
 
-function verifyArduinoProject():boolean {
-	if(!arduinoProject.isFolderArduinoProject()) {
-		vscode.window.showErrorMessage(`Not an Arduino Project.`);	
+function verifyArduinoProject(): boolean {
+	if (!arduinoProject.isFolderArduinoProject()) {
+		vscode.window.showErrorMessage(`Not an Arduino Project.`);
 		return false;
 	}
 	return true;
 }
 
 function executeArduinoCommand(command: string, args: string[], returnOutput: boolean = false): Promise<string | void> {
-    outputChannel.clear();
-    outputChannel.show(true);
-    outputChannel.appendLine('Running Arduino CLI...');
-    outputChannel.appendLine(`${command}`);
-    outputChannel.appendLine(args.join(' '));
+	outputChannel.clear();
+	outputChannel.show(true);
+	outputChannel.appendLine('Running Arduino CLI...');
+	outputChannel.appendLine(`${command}`);
+	outputChannel.appendLine(args.join(' '));
 
-    const child = cp.spawn(`${command}`, args);
-    let outputBuffer = '';  // String buffer to store output
+	const child = cp.spawn(`${command}`, args);
+	let outputBuffer = '';  // String buffer to store output
 
-    return new Promise((resolve, reject) => {
-        // Stream stdout to the output channel and optionally to the buffer
-        child.stdout.on('data', (data: Buffer) => {
-            const output = data.toString();
-            outputChannel.appendLine(output);
+	return new Promise((resolve, reject) => {
+		// Stream stdout to the output channel and optionally to the buffer
+		child.stdout.on('data', (data: Buffer) => {
+			const output = data.toString();
+			outputChannel.appendLine(output);
 
-            if (returnOutput) {
-                outputBuffer += output;
-            }
-        });
+			if (returnOutput) {
+				outputBuffer += output;
+			}
+		});
 
-        // Stream stderr to the output channel and optionally to the buffer
-        child.stderr.on('data', (data: Buffer) => {
-            const error = `Error: ${data.toString()}`;
-            outputChannel.appendLine(error);
+		// Stream stderr to the output channel and optionally to the buffer
+		child.stderr.on('data', (data: Buffer) => {
+			const error = `Error: ${data.toString()}`;
+			outputChannel.appendLine(error);
 
-            if (returnOutput) {
-                outputBuffer += error;
-            }
-        });
+			if (returnOutput) {
+				outputBuffer += error;
+			}
+		});
 
-        // Handle the process exit event
-        child.on('close', (code: number) => {
-            if (code === 0) {
-                // Successful completion
-                vscode.window.showInformationMessage('Command executed successfully.');
-                resolve(returnOutput ? outputBuffer : undefined);
-            } else {
-                // Command failed
-                vscode.window.showErrorMessage(`Command failed with code ${code}. Check Output window for details.`);
-                reject(`Command failed with code ${code}`);
-            }
-        });
+		// Handle the process exit event
+		child.on('close', (code: number) => {
+			if (code === 0) {
+				// Successful completion
+				vscode.window.showInformationMessage('Command executed successfully.');
+				resolve(returnOutput ? outputBuffer : undefined);
+			} else {
+				// Command failed
+				vscode.window.showErrorMessage(`Command failed with code ${code}. Check Output window for details.`);
+				reject(`Command failed with code ${code}`);
+			}
+		});
 
-        // Handle error event in case the command fails to start
-        child.on('error', (err) => {
-            vscode.window.showErrorMessage(`Failed to run command: ${err.message}`);
-            reject(`Failed to run command: ${err.message}`);
-        });
-    });
+		// Handle error event in case the command fails to start
+		child.on('error', (err) => {
+			vscode.window.showErrorMessage(`Failed to run command: ${err.message}`);
+			reject(`Failed to run command: ${err.message}`);
+		});
+	});
 }
 
 export function deactivate() { }
