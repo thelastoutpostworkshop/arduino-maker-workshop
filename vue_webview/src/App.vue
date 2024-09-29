ced4bdc32d3961b46a35446ede37421dd9f918e5
charles <cgiguere@iname.com>
2024-09-29 06:45:12 -0400

    toolbox

--- vue_webview/src/App.vue
+++ vue_webview/src/App.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts">
+      import Toolbox from './components/Toolbox.vue';
+
+</script>
+      
 <template>
   <v-app>
+    <Toolbox></Toolbox>
     <v-main>
       <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
-  //
-</script>
---
+++ vue_webview/src/components/Toolbox.vue
@@ -0,0 +1,12 @@
+<template>
+<v-navigation-drawer permanent rail>
+  <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+  <v-divider></v-divider>
+  <v-list-item prepend-icon="mdi-home" link></v-list-item>
+  <v-list-item prepend-icon="mdi-developer-board" link></v-list-item>
+</v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
