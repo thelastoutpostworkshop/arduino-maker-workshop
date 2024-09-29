f138856295d81b00b032c96ffb0c3de245f5346d
charles <cgiguere@iname.com>
2024-09-29 06:33:52 -0400

    webview

--- vue_webview/components.d.ts
+++ vue_webview/components.d.ts
@@ -8,5 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Toolbox: typeof import('./src/components/Toolbox.vue')['default']
   }
 }
