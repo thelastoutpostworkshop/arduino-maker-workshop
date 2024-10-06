7cf9ff38559cbddb5ae354813c628814e910f52c
charles <cgiguere@iname.com>
2024-10-06 13:43:04 -0400

    boards

--- vue_webview/src/components/Boards.vue
+++ vue_webview/src/components/Boards.vue
@@ -17,6 +17,9 @@ watch([() => vsCodeStore.boards], () => { }, { immediate: true });
 // Compute the board structure from the store
 const boardStructure = computed(() => vsCodeStore.boards?.boardStructure || {});
 
+function onBoardSelect(value: string) {
+  console.log("Selected Board FQBN:", value);
+}
 </script>
 
 <template>
@@ -27,21 +30,25 @@ const boardStructure = computed(() => vsCodeStore.boards?.boardStructure || {});
       </div>
       <v-text-field label="Current Board:" :model-value="vsCodeStore.boardConfiguration?.boardName" readonly>
         <template v-slot:loader>
-          <v-progress-linear :active="!vsCodeStore.boardConfiguration?.boardName" height="2"
-            indeterminate></v-progress-linear>
+          <v-progress-linear :active="!vsCodeStore.boardConfiguration?.boardName" height="2" indeterminate></v-progress-linear>
         </template>
       </v-text-field>
+
       <v-expansion-panels multiple>
+        <!-- Loop over each platform in the board structure -->
         <v-expansion-panel v-for="(boards, platform) in boardStructure" :key="platform">
           <v-expansion-panel-title>{{ platform }}</v-expansion-panel-title>
           <v-expansion-panel-text>
-            <v-autocomplete :items="boards.name" density="comfortable" label="Comfortable"></v-autocomplete>
-            <v-list>
-              <v-list-item v-for="board in boards" :key="board.fqbn">
-                <v-list-item-title>{{ board.name }}</v-list-item-title>
-                <v-list-item-subtitle>{{ board.fqbn }}</v-list-item-subtitle>
-              </v-list-item>
-            </v-list>
+            <!-- Autocomplete for selecting a board -->
+            <v-autocomplete
+              :items="boards"
+              item-title="name"
+              item-value="fqbn"
+              label="Select a Board"
+              @change="onBoardSelect"
+              outlined
+              dense
+            ></v-autocomplete>
           </v-expansion-panel-text>
         </v-expansion-panel>
       </v-expansion-panels>
