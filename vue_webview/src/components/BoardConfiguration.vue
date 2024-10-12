9ebdfeba4c10f55a4b841f7c669713295c6e2838
charles <cgiguere@iname.com>
2024-10-12 11:19:18 -0400

    board conf

--- vue_webview/src/components/BoardConfiguration.vue
+++ vue_webview/src/components/BoardConfiguration.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { useVsCodeStore } from '../stores/useVsCodeStore';
+import { ARDUINO_MESSAGES, WebviewToExtensionMessage } from '@shared/messages';
+import { onMounted, watch, computed, ref } from 'vue';
+
 const vsCodeStore = useVsCodeStore();
 
+const inDevelopment = computed(() => import.meta.env.DEV);
+
+function sendTestMessage() {
+  const message: WebviewToExtensionMessage = {
+    command: ARDUINO_MESSAGES.BOARD_CONFIGURATION,
+    errorMessage: "",
+    payload: import.meta.env.VITE_BOARDS_LISTALL_TEST
+  }
+  vsCodeStore.simulateMessage(message);
+}
 </script>
 
 <template>
     <v-container>
         <v-responsive>
+            <div v-if="inDevelopment">
+                <v-btn @click="sendTestMessage()">Send Test Message</v-btn>
+            </div>
             <div class="text-center">
                 <h1 class="text-h4 font-weight-bold">Board Configuration</h1>
             </div>
