c0a515884927c80efd9e2a8a5f7d308155cd83c4
charles <cgiguere@iname.com>
2024-10-12 07:52:40 -0400

    board configuration

---
+++ vue_webview/src/components/BoardConfiguration.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useVsCodeStore } from '../stores/useVsCodeStore';
+const vsCodeStore = useVsCodeStore();
+
+</script>
+
+<template>
+    <v-container>
+        <v-responsive>
+            <v-text-field label="Current Board:" :model-value="vsCodeStore.boardConfiguration?.boardName" readonly>
+                <template v-slot:loader>
+                    <v-progress-linear :active="!vsCodeStore.boardConfiguration?.boardName" height="2"
+                        indeterminate></v-progress-linear>
+                </template>
+            </v-text-field>
+        </v-responsive>
+    </v-container>
+</template>
