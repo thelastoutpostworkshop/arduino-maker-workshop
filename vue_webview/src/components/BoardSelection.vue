e5e2056f0a144ffa4498e8cfc5e38af5954c8789
charles <cgiguere@iname.com>
2024-10-26 11:03:04 -0400

    select board

--- vue_webview/src/components/BoardSelection.vue
+++ vue_webview/src/components/BoardSelection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { vscode } from '@/utilities/vscode';
 import { useVsCodeStore } from '../stores/useVsCodeStore';
-import { ARDUINO_MESSAGES, WebviewToExtensionMessage, BoardConfiguration, BoardList } from '@shared/messages';
+import { ARDUINO_MESSAGES, WebviewToExtensionMessage, BoardConfiguration } from '@shared/messages';
 import { onMounted, watch, computed, ref } from 'vue';
 
 const vsCodeStore = useVsCodeStore();
