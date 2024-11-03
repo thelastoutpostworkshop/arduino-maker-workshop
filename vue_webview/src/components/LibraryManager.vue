68efdb3406a3136ababeb0ce92333c936d0e8b95
charles <cgiguere@iname.com>
2024-11-03 09:02:54 -0500

    actions

--- vue_webview/src/components/LibraryManager.vue
+++ vue_webview/src/components/LibraryManager.vue
@@ -172,7 +172,7 @@ const inDevelopment = computed(() => import.meta.env.DEV);
             <v-tooltip :text="`Install latest ${item.name}`">
               <template v-slot:activator="{ props }">
                 <v-btn icon v-bind="props" variant="text">
-                  <v-icon class="me-2" size="small">
+                  <v-icon v-if="!isLibraryInstalled(item) || isLibraryUpdatable(item)" class="me-2" size="small">
                     mdi-tray-arrow-down
                   </v-icon>
                 </v-btn>
