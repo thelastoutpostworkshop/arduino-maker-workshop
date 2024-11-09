b717a57e487408f13e8cfbffff89e974fd3af06a
charles <cgiguere@iname.com>
2024-11-09 14:47:22 -0500

    board selection

--- vue_webview/src/components/BoardSelection.vue
+++ vue_webview/src/components/BoardSelection.vue
@@ -20,6 +20,8 @@ watch(
         store.sendMessage({ command: ARDUINO_MESSAGES.SET_BOARD, errorMessage: "", payload: newVal.fqbn });
         boardSelectBefore.value = [...boardSelect.value];
         store.boardOptions = null;
+        store.sendMessage({command: ARDUINO_MESSAGES.BOARD_OPTIONS, errorMessage: "", payload: ''})
+        store.sendMessage({command: ARDUINO_MESSAGES.ARDUINO_PROJECT_INFO, errorMessage: "", payload: ''})
       }
     });
   },
