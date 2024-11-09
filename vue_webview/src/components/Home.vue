8398d994bebf493833ca81b57b96297b68d8bf33
charles <cgiguere@iname.com>
2024-11-09 06:14:42 -0500

    Removed unused cards

--- vue_webview/src/components/Home.vue
+++ vue_webview/src/components/Home.vue
@@ -107,41 +107,6 @@ onMounted(() => {
             </v-select>
           </v-card>
         </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" href="https://vuetifyjs.com/"
-            prepend-icon="mdi-text-box-outline" rel="noopener noreferrer" rounded="lg"
-            subtitle="Learn about all things Vuetify in our documentation." target="_blank" title="Documentation"
-            variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides" prepend-icon="mdi-star-circle-outline"
-            rel="noopener noreferrer" rounded="lg" subtitle="Explore available framework Features." target="_blank"
-            title="Features" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant"
-            href="https://vuetifyjs.com/components/all" prepend-icon="mdi-widgets-outline" rel="noopener noreferrer"
-            rounded="lg" subtitle="Discover components in the API Explorer." target="_blank" title="Components"
-            variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant"
-            href="https://discord.vuetifyjs.com" prepend-icon="mdi-account-group-outline" rel="noopener noreferrer"
-            rounded="lg" subtitle="Connect with Vuetify developers." target="_blank" title="Community" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
       </v-row>
     </v-responsive>
   </v-container>
