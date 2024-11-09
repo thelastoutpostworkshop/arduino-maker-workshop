6d2bd9bf265bd439e7490884998cad8b93a3e5c6
charles <cgiguere@iname.com>
2024-11-09 14:30:14 -0500

    board configuration

--- vue_webview/src/components/BoardConfiguration.vue
+++ vue_webview/src/components/BoardConfiguration.vue
@@ -2,7 +2,9 @@
 import { useVsCodeStore } from '../stores/useVsCodeStore';
 import { ARDUINO_MESSAGES, ConfigOptionValue } from '@shared/messages';
 import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
 const store = useVsCodeStore();
 
 // Define boardOption as an object indexed by string keys
@@ -45,39 +47,41 @@ watch(
             <div class="text-center">
                 <h1 class="text-h4 font-weight-bold">Board Configuration</h1>
             </div>
-            <v-text-field label="Current Board:" :model-value="store.boardOptions?.name"
-                readonly>
-                <template v-slot:loader>
-                    <v-progress-linear :active="!store.boardOptions?.name" height="2"
-                        indeterminate></v-progress-linear>
-                </template>
-            </v-text-field>
-            <v-card class="pa-4" color="blue-grey-darken-4" rounded="lg">
-                <template v-slot:loader>
-                    <v-progress-linear :active="!store.boardOptions" height="2"
-                        indeterminate></v-progress-linear>
-                </template>
-                <template #title>
-                    <h2 class="text-h6 font-weight-bold">Board Options</h2>
-                </template>
+            <div v-if="store.projectInfo?.board">
+                <v-text-field label="Current Board:" :model-value="store.boardOptions?.name" readonly>
+                    <template v-slot:loader>
+                        <v-progress-linear :active="!store.boardOptions?.name" height="2"
+                            indeterminate></v-progress-linear>
+                    </template>
+                </v-text-field>
+                <v-card class="pa-4" color="blue-grey-darken-4" rounded="lg">
+                    <template v-slot:loader>
+                        <v-progress-linear :active="!store.boardOptions" height="2" indeterminate></v-progress-linear>
+                    </template>
+                    <template #title>
+                        <h2 class="text-h6 font-weight-bold">Board Options</h2>
+                    </template>
 
-                <template #subtitle v-if="store.boardOptions">
-                    <div class="text-subtitle-1"
-                        v-if="store.boardOptions?.config_options">
-                        Select your board options
+                    <template #subtitle v-if="store.boardOptions">
+                        <div class="text-subtitle-1" v-if="store.boardOptions?.config_options">
+                            Select your board options
+                        </div>
+                        <div class="text-subtitle-1" v-else>
+                            No options available for your board
+                        </div>
+                    </template>
+                    <div v-if="store.boardOptions?.config_options">
+                        <div v-for="(option) in store.boardOptions?.config_options" :key="option.option">
+                            <v-select v-model="boardOption[option.option]" :label="option.option_label"
+                                :items="option.values" item-title="value_label" item-value="value"
+                                return-object></v-select>
+                        </div>
                     </div>
-                    <div class="text-subtitle-1" v-else>
-                        No options available for your board
-                    </div>
-                </template>
-                <div v-if="store.boardOptions?.config_options">
-                    <div v-for="(option) in store.boardOptions?.config_options"
-                        :key="option.option">
-                        <v-select v-model="boardOption[option.option]" :label="option.option_label"
-                            :items="option.values" item-title="value_label" item-value="value" return-object></v-select>
-                    </div>
-                </div>
-            </v-card>
+                </v-card>
+            </div>
+            <div v-else>
+                <v-btn @click="router.push({ name: 'board-selection' })">Select a board first</v-btn>
+            </div>
 
         </v-responsive>
     </v-container>
