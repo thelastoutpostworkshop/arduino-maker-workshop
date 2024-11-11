4bbc74f7b662f3a6d1676642d0f18e04d34754e9
charles <cgiguere@iname.com>
2024-11-11 17:10:59 -0500

    install library version

--- vue_webview/src/stores/useVsCodeStore.ts
+++ vue_webview/src/stores/useVsCodeStore.ts
@@ -219,6 +219,13 @@ export const useVsCodeStore = defineStore('vsCode', {
                         console.log("Failed to parse library search response: " + error);
                     }
                     break;
+                case ARDUINO_MESSAGES.LIBRARY_VERSION_INSTALLED:
+                    this.libraryUpdating="";
+                    this.librariesInstalled=null;
+                    this.libraries=null;
+                    this.sendMessage({ command: ARDUINO_MESSAGES.CLI_LIBRARY_SEARCH, errorMessage: "", payload: "" });
+                    this.sendMessage({ command: ARDUINO_MESSAGES.CLI_LIBRARY_INSTALLED, errorMessage: "", payload: "" });
+                    break;
                 case ARDUINO_MESSAGES.CORE_UNINSTALLED:
                 case ARDUINO_MESSAGES.CORE_VERSION_INSTALLED:
                     this.boardUpdating = "";
