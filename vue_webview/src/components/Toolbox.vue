fcb78a89df0ccec3baff4e28873d8b7b94875261
charles <cgiguere@iname.com>
2024-11-17 15:02:48 -0500

    dot to indicated outdated board or libraries

--- vue_webview/src/components/Toolbox.vue
+++ vue_webview/src/components/Toolbox.vue
@@ -13,7 +13,17 @@ const router = useRouter()
         <v-list-item prepend-icon="mdi-home" v-tooltip  @click="router.push({ name: 'home' })">Project Home</v-list-item>
         <v-list-item prepend-icon="mdi-bulletin-board" v-tooltip  @click="router.push({ name: 'board-selection' })">Board Selection</v-list-item>
         <v-list-item prepend-icon="mdi-cog" v-tooltip  @click="router.push({ name: 'board-configuration' })">Board Configuration</v-list-item>
-        <v-list-item prepend-icon="mdi-developer-board" v-tooltip  @click="router.push({ name: 'board-manager' })">Boards Manager</v-list-item>
-        <v-list-item prepend-icon="mdi-library" v-tooltip  @click="router.push({ name: 'library-manager' })">Library Manager</v-list-item>
+        <v-list-item v-tooltip  @click="router.push({ name: 'board-manager' })">Boards Manager
+            <template v-slot:prepend>
+                <v-icon>mdi-developer-board</v-icon>
+                <v-badge color="yellow" dot></v-badge>
+            </template>
+        </v-list-item>
+        <v-list-item v-tooltip  @click="router.push({ name: 'library-manager' })">Library Manager
+            <template v-slot:prepend>
+                <v-icon>mdi-library</v-icon>
+                <v-badge color="yellow" dot></v-badge>
+            </template>  
+        </v-list-item>
     </v-navigation-drawer>
 </template>
