596f9a37bd247b3496322b89bd2cc14f98afa1cc
charles <cgiguere@iname.com>
2024-11-25 19:15:27 -0500

    cli

--- src/cli.ts
+++ src/cli.ts
@@ -164,7 +164,7 @@ export class ArduinoCLI {
 		return new Promise((resolve) => {
 			const arduinoVersionArgs = arduinoProject.getVersionArguments();
 
-			executeArduinoCommand(`${this.arduinoCLIPath}`, arduinoVersionArgs, true, false)
+			this.executeArduinoCommand(`${this.arduinoCLIPath}`, arduinoVersionArgs, true, false)
 				.then((result) => {
 					if (result) {
 						try {
@@ -206,7 +206,7 @@ export class ArduinoCLI {
 				throw new Error("Unable to get Board Configuration");
 			}
 			const configBoardArgs = arduinoProject.getBoardConfigurationArguments();
-			const result = await executeArduinoCommand(`${arduinoCLI.arduinoCLIPath}`, configBoardArgs, true, false);
+			const result = await this.executeArduinoCommand(`${arduinoCLI.arduinoCLIPath}`, configBoardArgs, true, false);
 
 			if (!result) {
 				window.showErrorMessage(`CLI : No result from get board configuration`);
@@ -230,7 +230,7 @@ export class ArduinoCLI {
 	): Promise<string> {
 		try {
 			const args = getArguments();
-			const result = await executeArduinoCommand(`${this.arduinoCLIPath}`, args, returnOutput, showOutput, channel, successMSG);
+			const result = await this.executeArduinoCommand(`${this.arduinoCLIPath}`, args, returnOutput, showOutput, channel, successMSG);
 			if (!result && returnOutput) {
 				const errorMsg = `${errorMessagePrefix}: No result`;
 				window.showErrorMessage(errorMsg);
@@ -286,7 +286,7 @@ export class ArduinoCLI {
 
 			// Use the full name (current directory + sketch name)
 			const args = arduinoProject.getNewSketchArguments(fullName);
-			const result = await executeArduinoCommand(`${arduinoCLI.arduinoCLIPath}`, args, true, false);
+			const result = await this.executeArduinoCommand(`${arduinoCLI.arduinoCLIPath}`, args, true, false);
 
 			if (!result) {
 				window.showErrorMessage(`CLI: No result from create new sketch`);
@@ -302,67 +302,63 @@ export class ArduinoCLI {
 			throw error;
 		}
 	}
-}
 
-
-
-
-
-export function executeArduinoCommand(command: string, args: string[], returnOutput: boolean = false, showOutput = true, channel: OutputChannel = arduinoCLIChannel, successMsg: string = ""): Promise<string | void> {
-	// outputChannel.clear();
-	if (showOutput) {
-		channel.show(true);
-	}
-	arduinoCLIChannel.appendLine('');
-	arduinoCLIChannel.appendLine('Running Arduino CLI...');
-	arduinoCLIChannel.appendLine(`${command}`);
-	arduinoCLIChannel.appendLine(args.join(' '));
-
-	const child = cp.spawn(`${command}`, args);
-	let outputBuffer = '';
-
-	return new Promise((resolve, reject) => {
-		// Stream stdout to the output channel and optionally to the buffer
-		child.stdout.on('data', (data: Buffer) => {
-			const output = data.toString();
-			if (showOutput) {
-				channel.append(output);
-			}
-
-			if (returnOutput) {
-				outputBuffer += output;
-			}
-		});
-
-		// Stream stderr to the output channel and optionally to the buffer
-		child.stderr.on('data', (data: Buffer) => {
-			const error = `Error: ${data.toString()}`;
-			if (showOutput) {
-				channel.appendLine(error);
-			}
-			if (returnOutput) {
-				outputBuffer += error;
-			}
-		});
-
-		child.on('close', (code: number) => {
-			if (code === 0) {
+	private executeArduinoCommand(command: string, args: string[], returnOutput: boolean = false, showOutput = true, channel: OutputChannel = arduinoCLIChannel, successMsg: string = ""): Promise<string | void> {
+		// outputChannel.clear();
+		if (showOutput) {
+			channel.show(true);
+		}
+		arduinoCLIChannel.appendLine('');
+		arduinoCLIChannel.appendLine('Running Arduino CLI...');
+		arduinoCLIChannel.appendLine(`${command}`);
+		arduinoCLIChannel.appendLine(args.join(' '));
+	
+		const child = cp.spawn(`${command}`, args);
+		let outputBuffer = '';
+	
+		return new Promise((resolve, reject) => {
+			// Stream stdout to the output channel and optionally to the buffer
+			child.stdout.on('data', (data: Buffer) => {
+				const output = data.toString();
 				if (showOutput) {
-					channel.appendLine('Command executed successfully.');
+					channel.append(output);
 				}
-				if (successMsg) {
-					window.showInformationMessage(successMsg);
+	
+				if (returnOutput) {
+					outputBuffer += output;
 				}
-				resolve(returnOutput ? outputBuffer : undefined);
-			} else {
-				channel.appendLine(`Command failed with code ${code}.`);
+			});
+	
+			// Stream stderr to the output channel and optionally to the buffer
+			child.stderr.on('data', (data: Buffer) => {
+				const error = `Error: ${data.toString()}`;
+				if (showOutput) {
+					channel.appendLine(error);
+				}
+				if (returnOutput) {
+					outputBuffer += error;
+				}
+			});
+	
+			child.on('close', (code: number) => {
+				if (code === 0) {
+					if (showOutput) {
+						channel.appendLine('Command executed successfully.');
+					}
+					if (successMsg) {
+						window.showInformationMessage(successMsg);
+					}
+					resolve(returnOutput ? outputBuffer : undefined);
+				} else {
+					channel.appendLine(`Command failed with code ${code}.`);
+					reject(undefined);
+				}
+			});
+	
+			child.on('error', (err: any) => {
+				channel.appendLine(`Failed to run command: ${err.message}`);
 				reject(undefined);
-			}
+			});
 		});
-
-		child.on('error', (err: any) => {
-			channel.appendLine(`Failed to run command: ${err.message}`);
-			reject(undefined);
-		});
-	});
+	}
 }
