e715bf767b8d0ba7ff7272da4a0b2993a9592322
charles <cgiguere@iname.com>
2024-12-01 07:28:05 -0500

    isPresent

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,6 @@ export class ArduinoConfiguration {
         return true;
     }
     private isPresent(): boolean {
-        let res: boolean = false;
         arduinoCLI.getArduinoConfig().then((json) => {
             try {
                 this.config = JSON.parse(json);
@@ -23,20 +22,18 @@ export class ArduinoConfiguration {
                     try {
                         arduinoExtensionChannel.appendLine("Creating new Arduino Configuration file");
                         // this.createNew();
-                        return true;
                     } catch (error) {
                         arduinoExtensionChannel.appendLine("Failed to create a new Arduino Configuration file");
                         return false;
                     }
                 }
             } catch (error) {
-                res = false;
+                return false;
             }
-            res = true;
         }).catch(() => {
-            res = false;
+            return false;
         });
-        return res;
+        return true;
     }
     private createNew() {
 
