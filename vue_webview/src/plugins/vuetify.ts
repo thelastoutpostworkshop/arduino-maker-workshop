5b79838dc0b0bb1583dd9fe2eac27a2dffc27ef4
charles <cgiguere@iname.com>
2024-12-05 16:41:20 -0500

    removed VTreeview

--- vue_webview/src/plugins/vuetify.ts
+++ vue_webview/src/plugins/vuetify.ts
@@ -7,7 +7,6 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
-import { VTreeview } from 'vuetify/labs/VTreeview'
 
 // Composables
 import { createVuetify } from 'vuetify'
