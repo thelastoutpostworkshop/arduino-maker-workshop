f19ebfd1478a22ee1befac53d7c7e9b95aa2c07c
charles <cgiguere@iname.com>
2024-12-07 09:06:13 -0500

    readConfig

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import { read } from "fs";
 import { arduinoCLI, arduinoExtensionChannel } from "./extension";
 import { ArduinoConfig } from "./shared/messages";
 const path = require('path');
@@ -7,24 +8,28 @@ const staging = 'staging';
 const arduino = 'Arduino';
 
 export class ArduinoConfiguration {
-    private _config:ArduinoConfig | null = null;
+    private _config: ArduinoConfig | null = null;
     constructor() {
 
     }
     public async verify(): Promise<boolean> {
         return this.isPresent();
     }
+    private async readConfig() {
+        const json = await arduinoCLI.getArduinoConfig();
+        this._config = JSON.parse(json);
+    }
     private async isPresent(): Promise<boolean> {
         try {
             const json = await arduinoCLI.getArduinoConfig();
             const parsedConfig: ArduinoConfig = JSON.parse(json);
-    
+
             // Ensure parsedConfig is valid before assigning
             if (parsedConfig && Object.keys(parsedConfig.config).length > 0) {
                 this._config = parsedConfig;
                 return true;
             }
-    
+
             // Handle case where config is empty
             arduinoExtensionChannel.appendLine("Creating new Arduino Configuration file");
             try {
@@ -39,7 +44,7 @@ export class ArduinoConfiguration {
             return false;
         }
     }
-    
+
     private async createNew() {
         const initJson = await arduinoCLI.initArduinoConfiguration();
         const config = JSON.parse(initJson);
@@ -49,18 +54,19 @@ export class ArduinoConfiguration {
         await arduinoCLI.setConfigDownloadDirectory(downloadPath);
         await arduinoCLI.setConfigDataDirectory(configPath);
         await arduinoCLI.setConfigUserDirectory(arduinoDir);
+        await this.readConfig();
     }
     private getDocumentsFolderPath() {
-		const homeDir = os.homedir(); // Get the user's home directory
+        const homeDir = os.homedir(); // Get the user's home directory
 
-		switch (os.platform()) {
-			case 'win32':
-				return path.join(homeDir, 'Documents'); // Windows path to Documents
-			case 'linux':
-			case 'darwin':
-				return path.join(homeDir, 'Documents'); // Linux and macOS path to Documents
-			default:
-				throw new Error('Unsupported platform');
-		}
-	}
+        switch (os.platform()) {
+            case 'win32':
+                return path.join(homeDir, 'Documents'); // Windows path to Documents
+            case 'linux':
+            case 'darwin':
+                return path.join(homeDir, 'Documents'); // Linux and macOS path to Documents
+            default:
+                throw new Error('Unsupported platform');
+        }
+    }
 }
