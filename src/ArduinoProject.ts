b624c324a95ac0f3d58c0817895530ea88f5966f
charles <cgiguere@iname.com>
2025-07-29 16:43:29 -0400

    setPort

--- src/ArduinoProject.ts
+++ src/ArduinoProject.ts
@@ -169,13 +169,11 @@ export class ArduinoProject {
         if (this.configJson.monitorPortSettings.port.trim().length === 0) {
             arduinoExtensionChannel.appendLine(`Monitor port is empty. Default to ${this.configJson.port}`);
             this.configJson.monitorPortSettings.port = this.configJson.port;
-            if (VueWebviewPanel.currentPanel) {
-                VueWebviewPanel.sendMessage({
-                    command: ARDUINO_MESSAGES.ARDUINO_PROJECT_INFO,
-                    errorMessage: "",
-                    payload: this.configJson
-                })
-            }
+            VueWebviewPanel.sendMessage({
+                command: ARDUINO_MESSAGES.ARDUINO_PROJECT_INFO,
+                errorMessage: "",
+                payload: this.configJson
+            })
         }
         this.writeVSCodeArduinoConfiguration();
     }
