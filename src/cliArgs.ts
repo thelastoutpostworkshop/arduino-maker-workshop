d306b13a4791ffab4d8b27bf23f0121f5958adfd
charles <cgiguere@iname.com>
2025-07-31 17:53:31 -0400

    removed unused imports

--- src/cliArgs.ts
+++ src/cliArgs.ts
@@ -1,7 +1,6 @@
 import { arduinoProject, arduinoYaml } from "./extension";
 import { window, workspace } from 'vscode';
-import { BUILD_NAME_PROFILE, DEFAULT_PROFILE, PROFILES_STATUS, UNKNOWN_PROFILE } from "./shared/messages";
-import { profile } from "console";
+import { DEFAULT_PROFILE, PROFILES_STATUS, UNKNOWN_PROFILE } from "./shared/messages";
 
 const path = require('path');
 
@@ -315,11 +314,11 @@ export class CLIArguments {
         if (arduinoYaml.status() == PROFILES_STATUS.ACTIVE) {
             // Compile using a profile
             const profileName = arduinoProject.getCompileProfile();
-            if(!profileName) {
+            if (!profileName) {
                 window.showWarningMessage("You must select a profile");
                 return [];
             }
-            if(profileName !== DEFAULT_PROFILE) {
+            if (profileName !== DEFAULT_PROFILE) {
                 compileCommand.push(profileOption);
                 compileCommand.push(profileName);
             }
