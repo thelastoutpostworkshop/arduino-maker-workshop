655b57678512c768bcb2fe1e1b0f2247ef10be7d
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2025-09-05 16:40:29 -0400

    _getWebviewContent

--- src/VueWebviewPanel.ts
+++ src/VueWebviewPanel.ts
@@ -402,7 +402,14 @@ export class VueWebviewPanel {
           <head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'nonce-${nonce}'; font-src ${webview.cspSource}; img-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+            <meta http-equiv="Content-Security-Policy" content="
+            default-src 'none';
+            img-src ${webview.cspSource} https: data:;
+            font-src ${webview.cspSource} https: data:;
+            style-src ${webview.cspSource} 'unsafe-inline';
+            script-src 'nonce-${nonce}';
+            connect-src ${webview.cspSource} https:;
+            ">
             <link rel="stylesheet"type="text/css" href="${stylesUri}">
             <title>Arduino</title>
         </head>
